79f17700c29765b28a4cf5bd1b6d90bc7e3eb7f2
Ryan Mueller <33radar@gmail.com>
2026-07-03 22:44:53 -0500

    Overwrite image with exiftools

--- RawForge/application/helpers/exiftools.py
+++ RawForge/application/helpers/exiftools.py
@@ -7,8 +7,7 @@ def copy_with_exiftools(source_img, target_img, verbose):
     if is_exiftool_installed():
         import exiftool
         with exiftool.ExifToolHelper() as et:
-            et.execute("-tagsFromFile", source_img, target_img)
+            et.execute( "-overwrite_original", "-tagsFromFile", source_img, target_img)
             if verbose > 1: print(f"Metadata copied from {source_img} to {target_img}")    
     else:
         if verbose > 1: print(f"Did not find exiftool")    
-
